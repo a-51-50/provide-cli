8ab2ec6fcde79804b9f9c1a5aff9bd2df5c58878
Kyle Thomas <k.thomas@unmarkedconsulting.com>
2018-08-26 06:55:39 -0400

    WIP for goxc

--- cmd/root.go
+++ cmd/root.go
@@ -10,6 +10,8 @@ import (
 	"github.com/spf13/viper"
 )
 
+var VERSION string
+
 var cfgFile string
 var verbose bool
 
--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import "github.com/provideservices/provide-cli/cmd"
 
+// VERSION set during build
+var VERSION = "0.0.0"
+
 func main() {
-	cmd.Execute()
+	cmd.Execute(VERSION)
 }
